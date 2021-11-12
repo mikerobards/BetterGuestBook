7d43a80e1ef7163bd7f9c310537684c1bb03fbe9
Michael Robards <mikerobards@icloud.com>
2021-11-12 11:12:21 -0500

    guest book works

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using GuestBookLibrary.Models;
+
+
+// Capture the info about each guest (assumption at least one guest and unknown maximum)
+// Info to capture: First name, Last name, message to the host
+// Once done capturing, loop thru each guest and print their info
 
 namespace ConsoleUI
 {
@@ -7,6 +14,40 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Better Guest Book!");
+
+            List<GuestModel> guests = new List<GuestModel>();
+            // using a List because you can keep adding to it
+
+            // loop through each guest
+
+            string moreGuestsComing = "";
+
+            do // run the code at least once
+            {
+                GuestModel guest = new();
+
+                Console.Write("What is your first name: ");
+                guest.FirstName = Console.ReadLine();
+
+                Console.Write("What is your last name: ");
+                guest.LastName = Console.ReadLine();
+
+                Console.Write("What is your message to the host: ");
+                guest.MessageToHost = Console.ReadLine();
+
+                Console.Write("Are more guests coming? (y/n) ");
+                moreGuestsComing = Console.ReadLine();
+
+                guests.Add(guest);
+
+                Console.Clear();
+            } while (moreGuestsComing.ToLower() == "y");
+
+            foreach (GuestModel guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
+
         }
     }
 }
